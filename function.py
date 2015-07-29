a28208e6d944da8679450432cf67fae2d85fb453
Akhil Nair <akhiln007@gmail.com>
2015-07-29 21:57:00 +0100

    ver .1

---
+++ function.py
@@ -0,0 +1,60 @@
+
+print("Happy Birthday to you!")
+print("Happy Birthday to you!")
+print("Happy Birthday, dear Akhil.")
+print("Happy Birthday to you!")
+
+
+def HappyBirthdayAkhil():
+	print("Happy Birthday to you!")
+	print("Happy Birthday to you!")
+	print("Happy Birthday, dear First Fun.")
+	print("Happy Birthday to you!")
+
+
+def HappyBirthdayNikhil():
+	print("Happy Birthday to you!")
+	print("Happy Birthday to you!")
+	print("Happy Birthday, dear Second Fun.")
+	print("Happy Birthday to you!")
+
+HappyBirthdayAkhil()
+HappyBirthdayNikhil()	
+
+
+
+def HappyBirthdayAkhil():
+	print("Happy Birthday to you!")
+	print("Happy Birthday to you!")
+	print("Happy Birthday, dear First Fun in main.")
+	print("Happy Birthday to you!")
+
+
+def HappyBirthdayNikhil():
+	print("Happy Birthday to you!")
+	print("Happy Birthday to you!")
+	print("Happy Birthday, dear Second Fun in main.")
+	print("Happy Birthday to you!")
+
+def main():
+ HappyBirthdayAkhil()
+ HappyBirthdayNikhil()	
+
+main()
+
+
+
+
+
+def HappyBirthday(person):
+	print("Happy Birthday to you!")
+	print("Happy Birthday to you!")
+	print("Happy Birthday, dear",person)
+	print("Happy Birthday to you!")
+
+
+def main():
+ HappyBirthday('First Argument')
+ HappyBirthday('Second Argument')	
+
+main()
