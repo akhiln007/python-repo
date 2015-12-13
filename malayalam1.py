19d49984096254a4d6df491a4b0309b048c6dfa4
Akhil Nair <akhiln007@gmail.com>
2015-12-13 19:48:23 +0000

    Encapsulating dictionary

---
+++ malayalam1.py
@@ -0,0 +1,17 @@
+
+
+def CreateDict():
+ mal=dict()
+ mal['one']='onnu'
+ mal['two']='randu'
+ mal['three']='munnu'
+ mal['four']='naalu'
+ mal['five']='anchu'
+ return mal 
+
+def main():
+ dictionary=CreateDict()
+ print(dictionary['one'])
+ print(dictionary['two'])
+
+main()
