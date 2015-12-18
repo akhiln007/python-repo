6141a36a74118691287f6fcaf9026c1aaf7122ab
Akhil Nair <akhiln007@gmail.com>
2015-12-18 22:19:05 +0000

    List execution using functions

---
+++ numberlist.py
@@ -0,0 +1,15 @@
+def NumberList(items):
+ n=1
+ for i in items:
+  print(n,i)
+  n=n+1
+
+
+def main():
+ NumberList(['Banana','Pear','Orange','Apple'])
+ print()
+ NumberList(['Carrot','BeetRoot','Cauliflower','Cucumber'])
+
+main()
+
+
