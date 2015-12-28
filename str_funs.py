c0f5cbe4ef4057d6206542b8d443ae4d3c0a7b99
Akhil Nair <akhiln007@gmail.com>
2015-12-28 07:24:37 +0000

    Strings related functions

---
+++ str_funs.py
@@ -0,0 +1,12 @@
+s="I am learning python"
+print(s.count('a'))
+print(s[:4])
+print(s[14:])
+print(s[:])
+print(s.find('r'))
+print(s.split())
+print(s.split('a'))
+print(s.split(':'))
+print(s.join('p'))
+
+
