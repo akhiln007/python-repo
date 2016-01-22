990283fde2b5f34634d83e5ff31fc32a2413bf1b
Akhil Nair <akhiln007@gmail.com>
2016-01-22 09:37:38 +0000

    Number is armstrong number or not

--- armstrong.py
+++ armstrong.py
@@ -1,9 +1,9 @@
 for num in range(100,1000):
-t=num
-cube=0
-while t > 0:
+ t=num
+ cube=0
+ while t > 0:
   a=t%10
   cube=cube+pow(a,3)
   t=t/10
-if cube==num:
+  if cube==num:
    print(num,"is Armstrong")
